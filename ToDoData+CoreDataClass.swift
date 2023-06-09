3a5d3bb624698317472190a132ccda644be436e5
DanjyChichi <td0413@daum.net>
2023-06-10 03:52:36 +0900

    message

---
+++ ToDoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoData+CoreDataClass.swift
+//  MyToDoApp
+//
+//  Created by 박상우 on 2023/04/05.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoData)
+public class ToDoData: NSManagedObject {
+
+}
